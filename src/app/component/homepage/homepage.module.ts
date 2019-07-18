1ccdef1cabf54df5d8d97f83ab5ff8161e8f1bfe
Erick Tepale <e.tepale@gmail.com>
2019-07-18 19:23:05 -0400

    GOT THE MENU IN THE HOMEPAGE !

--- src/app/component/homepage/homepage.module.ts
+++ src/app/component/homepage/homepage.module.ts
@@ -7,6 +7,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { HomepagePage } from './homepage.page';
+import { MenuPageModule } from '../menu/menu.module';
+import { MenuPage } from '../menu/menu.page';
 
 
 const routes: Routes = [
@@ -21,7 +23,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MenuPageModule
   ],
   declarations: [HomepagePage]
 })
